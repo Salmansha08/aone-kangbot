50960244ae84a33516a3ef4ea9ec62ad40748b9e
Angga <angga@linuxmail.org>
2020-07-08 23:22:01 +0700

    covid: change data source and improve output
    Signed-off-by: Angga <angga@linuxmail.org>
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -8,15 +8,25 @@ from userbot.events import register
 #@borg.on(admin_cmd(pattern="coronavirus (.*)"))
 #async def _(event):
 @register(outgoing=True, pattern="^.covid (.*)")
-async def cv(event):
-    covid = Covid()
-    data = covid.get_data()
+async def corona(event):
+    await event.edit("`Processing...`")
     country = event.pattern_match.group(1)
-    country_data = get_country_data(country, data)
-    output_text = "" 
-    for name, value in country_data.items():
-        output_text += "`{}`: `{}`\n".format(str(name), str(value))
-    await event.edit("**CoronaVirus Info in {}**:\n\n{}".format(country.capitalize(), output_text))
+    covid = Covid(source="worldometers")
+    country_data = covid.get_status_by_country_name(country)
+    if country_data:
+        output_text =  f"`Confirmed   : {country_data['confirmed']}`\n"
+        output_text += f"`Active      : {country_data['active']}`\n"
+        output_text += f"`Deaths      : {country_data['deaths']}`\n"
+        output_text += f"`Recovered   : {country_data['recovered']}`\n"
+        output_text += f"`New Cases   : {country_data['new_cases']}`\n"
+        output_text += f"`New Deaths  : {country_data['new_deaths']}`\n"
+        output_text += f"`Critical    : {country_data['critical']}`\n"
+        output_text += f"`Total Tests : {country_data['total_tests']}`\n\n"
+        output_text += f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})"
+    else:
+        output_text = "No information yet about this country!"
+    await event.edit(f"Corona Virus Info in {country}:\n\n{output_text}")
+
 
 def get_country_data(country, world):
     for country_data in world:
